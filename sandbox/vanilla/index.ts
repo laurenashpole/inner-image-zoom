c2f09313e3a165b413c782ca7a31528cf01555a8
Lauren Ashpole <lauren@laurenashpole.com>
2023-07-16 19:26:32 -0400

    Added examples to sandbox

--- sandbox/vanilla/index.ts
+++ sandbox/vanilla/index.ts
@@ -1,24 +1,62 @@
 import InnerImageZoom from '../../packages/vanilla/src';
 import '../../packages/vanilla/src/styles.css';
 
-new InnerImageZoom('.iiz', { fullscreenOnMobile: true });
+new InnerImageZoom('.iiz');
 
 new InnerImageZoom('#iizFigure', {
-  moveType: 'drag',
   zoomSrc: 'https://images.unsplash.com/photo-1609074405294-355851aead3e?fit=crop&w=1500&q=80'
 });
 
-new InnerImageZoom('.iiz-multi');
+new InnerImageZoom('.iiz-hover', {
+  zoomType: 'hover',
+  zoomPreload: true
+});
+
+new InnerImageZoom('.iiz-drag', {
+  moveType: 'drag',
+});
+
+new InnerImageZoom('.iiz-fullscreen', {
+  fullscreenOnMobile: true,
+  mobileBreakpoint: 600
+});
+
+new InnerImageZoom('.iiz-hide', {
+  hideCloseButton: true,
+  hideHint: true
+});
+
+const callbackHeading = document.querySelector('.callbackHeading');
+
+new InnerImageZoom('.iiz-callbacks', {
+  afterZoomIn: function () {
+    if (callbackHeading && callbackHeading['style']) {
+      callbackHeading['style'].color = 'green';
+    }
+  },
+  afterZoomOut: function () {
+    if (callbackHeading && callbackHeading['style']) {
+      callbackHeading['style'].color = 'black';
+    }
+  }
+});
 
 const iizInits = new InnerImageZoom('.iiz-inits');
+const uninitBtn = document.getElementById('uninit');
+const reinitBtn = document.getElementById('reinit');
 
-document.getElementById('uninit')?.addEventListener('click', () => {
-  iizInits.uninit();
-});
-
-document.getElementById('reinit')?.addEventListener('click', () => {
-  iizInits.reinit({
-    moveType: 'pan',
-    zoomSrc: 'https://images.unsplash.com/photo-1560114928-40f1f1eb26a0?fit=crop&w=1500&q=80'
+if (uninitBtn) {
+  uninitBtn.addEventListener('click', function () {
+    iizInits.uninit();
   });
-});
+}
+
+if (reinitBtn) {
+  reinitBtn.addEventListener('click', function () {
+    iizInits.reinit({
+      moveType: 'drag',
+      zoomSrc: 'https://images.unsplash.com/photo-1560114928-40f1f1eb26a0?fit=crop&w=1500&q=80'
+    });
+  });
+}
+
