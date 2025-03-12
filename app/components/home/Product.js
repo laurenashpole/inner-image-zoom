f97c6fe80df6a2a11735520396247f7372c9fc69
Lauren Ashpole <lauren.ashpole@getcatch.com>
2025-03-12 05:42:45 -0400

    feat: add footer and subfooter

--- app/components/home/Product.js
+++ app/components/home/Product.js
@@ -9,7 +9,6 @@ import { InnerImageZoom } from 'react-inner-image-zoom';
 import {
   Box,
   Button,
-  Container,
   List,
   ListItem,
   SimpleGrid,
@@ -17,140 +16,138 @@ import {
   Title,
 } from '@mantine/core';
 
+import Section from '../layout/Section';
+
 import 'react-inner-image-zoom/lib/InnerImageZoom/styles.css';
 import styles from './Product.module.css';
 
 const Product = () => {
   return (
-    <Box className={styles.root}>
-      <Container
-        size="lg"
-        px="lg"
-        py={{
-          base: 'xxxl',
-          sm: 'xxxxl',
-          md: 'xxxxxl',
+    <Section
+      className={styles.root}
+      py={{
+        base: 'xxxl',
+        sm: 'xxxxl',
+        md: 'xxxxxl',
+      }}
+    >
+      <SimpleGrid
+        cols={{ base: 1, sm: 2 }}
+        spacing={{
+          base: 'xxl',
+          md: 'xxxl',
+        }}
+        style={{
+          alignItems: 'flex-start',
         }}
       >
-        <SimpleGrid
-          cols={{ base: 1, sm: 2 }}
-          spacing={{
-            base: 'xxl',
-            md: 'xxxl',
-          }}
+        <Box
           style={{
-            alignItems: 'flex-start',
+            borderRadius: '0.5rem',
+            overflow: 'hidden',
           }}
         >
-          <Box
-            style={{
-              borderRadius: '0.5rem',
-              overflow: 'hidden',
+          <InnerImageZoom
+            src="/home-pdp.jpg"
+            zoomScale={0.5}
+            width={1200}
+            height={1200}
+            imgAttributes={{
+              alt: 'Example zoom photo of a sideboard and photo wall',
             }}
+          />
+        </Box>
+
+        <Stack
+          mr={{
+            sm: 'calc(var(--mantine-spacing-xxl) - var(--mantine-spacing-lg))',
+            md: 'calc(var(--mantine-spacing-xxxl) - var(--mantine-spacing-lg))',
+          }}
+        >
+          <Title order={2} size="h1">
+            Perfect for product pages
+          </Title>
+
+          <Title order={3} fs="italic">
+            $0
+          </Title>
+
+          <Button
+            size="xl"
+            my="lg"
+            color="green"
+            component="a"
+            href="https://github.com/laurenashpole/inner-image-zoom"
+            fz="md"
           >
-            <InnerImageZoom
-              src="/home-pdp.jpg"
-              zoomScale={0.5}
-              width={1200}
-              height={1200}
-              imgAttributes={{
-                alt: 'Example zoom photo of a sideboard and photo wall',
-              }}
-            />
-          </Box>
+            View on GitHub
+          </Button>
 
-          <Stack
-            mr={{
-              sm: 'calc(var(--mantine-spacing-xxl) - var(--mantine-spacing-lg))',
-              md: 'calc(var(--mantine-spacing-xxxl) - var(--mantine-spacing-lg))',
-            }}
-          >
-            <Title order={2} size="h1">
-              Perfect for product pages
-            </Title>
+          <Title order={3} size="" tt="uppercase">
+            Details
+          </Title>
 
-            <Title order={3} fs="italic">
-              $0
-            </Title>
-
-            <Button
-              size="xl"
-              my="lg"
-              color="green"
-              component="a"
-              href="https://github.com/laurenashpole/inner-image-zoom"
-              fz="md"
+          <List spacing="md">
+            <ListItem
+              icon={
+                <HiCursorClick
+                  style={{
+                    width: '1rem',
+                    height: '1rem',
+                    marginRight: '0.5rem',
+                  }}
+                />
+              }
             >
-              View on GitHub
-            </Button>
+              Zoom on click or hover
+            </ListItem>
 
-            <Title order={3} size="" tt="uppercase">
-              Details
-            </Title>
+            <ListItem
+              icon={
+                <FiMove
+                  style={{
+                    width: '1rem',
+                    height: '1rem',
+                    marginRight: '0.5rem',
+                  }}
+                />
+              }
+            >
+              Move by dragging on touch devices and dragging or panning on hover
+              on non-touch devices
+            </ListItem>
 
-            <List spacing="md">
-              <ListItem
-                icon={
-                  <HiCursorClick
-                    style={{
-                      width: '1rem',
-                      height: '1rem',
-                      marginRight: '0.5rem',
-                    }}
-                  />
-                }
-              >
-                Zoom on click or hover
-              </ListItem>
+            <ListItem
+              icon={
+                <BsImageAlt
+                  style={{
+                    width: '0.75rem',
+                    height: '0.75rem',
+                    marginRight: '0.5rem',
+                  }}
+                />
+              }
+            >
+              Supports responsive images
+            </ListItem>
 
-              <ListItem
-                icon={
-                  <FiMove
-                    style={{
-                      width: '1rem',
-                      height: '1rem',
-                      marginRight: '0.5rem',
-                    }}
-                  />
-                }
-              >
-                Move by dragging on touch devices and dragging or panning on
-                hover on non-touch devices
-              </ListItem>
-
-              <ListItem
-                icon={
-                  <BsImageAlt
-                    style={{
-                      width: '0.75rem',
-                      height: '0.75rem',
-                      marginRight: '0.5rem',
-                    }}
-                  />
-                }
-              >
-                Supports responsive images
-              </ListItem>
-
-              <ListItem
-                icon={
-                  <LuLibrary
-                    style={{
-                      width: '1rem',
-                      height: '1rem',
-                      marginRight: '0.5rem',
-                    }}
-                  />
-                }
-              >
-                Can be used with popular libraries like Swiper and React Lazy
-                Load
-              </ListItem>
-            </List>
-          </Stack>
-        </SimpleGrid>
-      </Container>
-    </Box>
+            <ListItem
+              icon={
+                <LuLibrary
+                  style={{
+                    width: '1rem',
+                    height: '1rem',
+                    marginRight: '0.5rem',
+                  }}
+                />
+              }
+            >
+              Can be used with popular libraries like Swiper and React Lazy Load
+            </ListItem>
+          </List>
+        </Stack>
+      </SimpleGrid>
+    </Section>
   );
 };
 
